c72f171e48dc341171c9cbfe68b20ba5ff86c099
Amar Kumar Singh <developeramarkumar@Amars-MacBook-Pro-2.local>
2024-02-13 18:44:36 +0530

    open close

---
+++ solid/open-close/index.ts
@@ -0,0 +1,37 @@
+namespace OpenClosePrinciple {
+	type OrderType = number[];
+	interface OrderProcessorInterface {
+		confirm(): void;
+	}
+	class OrderProcessor implements OrderProcessorInterface{
+		protected orderItems: OrderType;
+
+		constructor(orderItems: OrderType) {
+			this.orderItems = orderItems;
+		}
+
+		confirm(): void {
+			console.log('Order confirmed for :', this.orderItems);
+		}
+
+		addMoreOrder(orderId: number): void {
+			this.orderItems.push(orderId);
+			console.log('New order added for :', orderId);
+		}
+	}
+
+	class OrderProcessorWithInvoice extends OrderProcessor {
+
+		generateInvoice(): void {
+			console.log('Order invoice generated for :', this.orderItems);
+		}
+
+	}
+
+	const orderItems = [1, 2, 3];
+
+	const orderProcessor = new OrderProcessorWithInvoice(orderItems);
+	orderProcessor.addMoreOrder(4)
+	orderProcessor.confirm();
+	orderProcessor.generateInvoice();
+}
--- solid/single-responsibility/index.ts
+++ solid/single-responsibility/index.ts
@@ -1,36 +1,38 @@
-type OrderType = number[];
+namespace SingleResponsibility {
+	type OrderType = number[];
 
-class OrderProcessor {
-  private orderItems: OrderType;
+	class OrderProcessor {
+		private orderItems: OrderType;
 
-  constructor(orderItems: OrderType) {
-    this.orderItems = orderItems;
-  }
+		constructor(orderItems: OrderType) {
+			this.orderItems = orderItems;
+		}
 
-  confirm(): void {
-    console.log('Order confirmed for :', this.orderItems);
-  }
+		confirm(): void {
+			console.log('Order confirmed for :', this.orderItems);
+		}
+	}
+
+	class DeliveryOrder {
+		changeStatus(orderItems: OrderType): void {
+			console.log('delivery status changing for :', orderItems);
+		}
+	}
+
+	class InvoiceGenerator {
+		generateInvoice(orderItems: OrderType): void {
+			console.log('Invoice generated for:', orderItems);
+		}
+	}
+
+	const orderItems = [1, 2, 3];
+
+	const orderProcessor = new OrderProcessor(orderItems);
+	orderProcessor.confirm();
+
+	const deliveryOrder = new DeliveryOrder();
+	deliveryOrder.changeStatus(orderItems);
+
+	const invoiceGenerator = new InvoiceGenerator();
+	invoiceGenerator.generateInvoice(orderItems);
 }
-
-class DeliveryOrder {
-  changeStatus(orderItems: OrderType): void {
-    console.log('delivery status changing for :', orderItems);
-  }
-}
-
-class InvoiceGenerator {
-  generateInvoice(orderItems: OrderType): void {
-    console.log('Invoice generated for:', orderItems);
-  }
-}
-
-const orderItems = [1, 2, 3];
-
-const orderProcessor = new OrderProcessor(orderItems);
-orderProcessor.confirm();
-
-const deliveryOrder = new DeliveryOrder();
-deliveryOrder.changeStatus(orderItems);
-
-const invoiceGenerator = new InvoiceGenerator();
-invoiceGenerator.generateInvoice(orderItems);
